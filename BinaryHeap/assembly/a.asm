0902dc108a8920afdb2639084b96b6e7757dd98f
Mikołaj Truszczyński <m.truszczynski04@gmail.com>
2024-10-05 15:11:43 +0200

    insert to heap in assembly works, and the heap building is complete :)

--- BinaryHeap/assembly/a.asm
+++ BinaryHeap/assembly/a.asm
@@ -53,13 +53,13 @@ section .text                                       ; declaring section .text wh
         SYSCALL                                     ; calling close syscall
 
         ;INITIALIZE BINARY HEAP WITH 64 quadword capacity
-        mov qword [heap_capacity], 0x200            ; setting heap_capacity to 512 bytes (2 * 16^2 = 512)
+        mov qword [heap_capacity], 0x201            ; setting heap_capacity to 512 bytes (2 * 16^2 = 512)
         mov qword [heap_size], 0x00                 ; setting heap_size to 0
         mov rax, 0x09                               ; setting syscall to: mmap
         xor rdi, rdi                                ; rdi in MMAP is address where we want to map file, but as we set it to 0, kernel will choose address for us
         mov rsi, [heap_capacity]                    ; setting size of mapped memory to heap_capacity
         mov rdx, 0x03                               ; setting memory protection to PROT_READ | PROT_WRITE (it's mode for read and write memory)
-        mov r10, 0x21                               ; setting flag for mapping as MAP_PRIVATE (changes made to memory mapped file will not be visible to other processes)
+        mov r10, 0x01                               ; setting flag for mapping as MAP_PRIVATE (changes made to memory mapped file will not be visible to other processes)
         xor r8, r8                                  ; setting file_descriptor to 0 (as we are not mapping file)
         xor r9, r9                                  ; setting offset in file to 0 (as we are not mapping file)
         SYSCALL                                     ; calling mmap syscall - we are mapping memory for heap and getting address of mapped memory in rax
@@ -67,13 +67,12 @@ section .text                                       ; declaring section .text wh
 
         ;ITERATE THROUGH FILE
         xor rcx, rcx                                ; rcx = current index in our iteration (we start from 0)
-        mov r15, [file_pointer]                     ; rsi = pointer to the file
+        mov rsi, [file_pointer]                     ; rsi = pointer to the file
         xor rax, rax                                ; rax = value of the number we are reading
         file_iteration_loop:                        ; we are iterating through every byte in the file
             cmp rcx, [file_size]                    ; if we have reached the end of the file, we exit the loop
             jge end_loop                            ; jump to the end of the loop
-            mov bl, [r15 + rcx]                     ; bl is 8 lowest bytes from rbx register, and it holds the current byte we are reading
-            add rcx, 0x01
+            mov bl, [rsi + rcx]                     ; bl is 8 lowest bytes from rbx register, and it holds the current byte we are reading
 
             movzx rdx, bl                           ; we are moving the 8 bytes bl to 64 bytes register so we can do some operations
             cmp rdx, 0x0a                           ; if we have reached the end of the line, we jump to new_line
@@ -83,19 +82,19 @@ section .text                                       ; declaring section .text wh
             imul rax, rax, 0x0a                     ; we are multiplying the current value by 10
             add rax, rdx                            ; we are adding the new digit to the current value
 
-
+            inc rcx                                 ; after reading the byte, we increment the index
             jmp file_iteration_loop                 ; jump to the beginning of the loop
 
         new_line:                                   ; we have reached the end of the line
-            call insert_to_heap                     ; we are inserting the value to the heap
+            ;call insert_to_heap                     ; we are inserting the value to the heap
             xor rax, rax                            ; we are resetting the value of the number we are reading
 
-
+            inc rcx                                 ; after reading the byte, we increment the index
             jmp file_iteration_loop                 ; we are jumping to the beginning of the loop
 
         end_loop:                                   ; we have reached the end of the file
 
-            call insert_to_heap                     ; we are inserting the last value to the heap
+            ;call insert_to_heap                     ; we are inserting the last value to the heap
 
 
         call clean_and_exit                         ; we are cleaning up and exiting our program
@@ -103,7 +102,6 @@ section .text                                       ; declaring section .text wh
 
 
     clean_and_exit:
-
         ;MUNMAP file
         mov rax, 0x0b
         mov rdi, [file_pointer]
@@ -118,15 +116,18 @@ section .text                                       ; declaring section .text wh
 
         ;EXIT
         mov rax, 0x3c
-        mov rdi, 0x00
+        mov rdi, rcx
         SYSCALL
     ret
 
     resize_heap:
 
-        mov r8, rcx
-        mov r12, [heap_pointer]
-        mov r13, [heap_capacity]
+
+        ;MUNMAP current heap
+        mov rax, 0x0b
+        mov rdi, [heap_pointer]
+        mov rsi, [heap_capacity]
+        SYSCALL
 
         ;DOUBLE CAPACITY
         mov rax, [heap_capacity]
@@ -138,36 +139,19 @@ section .text                                       ; declaring section .text wh
         xor rdi, rdi
         mov rsi, [heap_capacity]
         mov rdx, 0x03
-        mov r10, 0x21
+        mov r10, 0x01
         xor r9, r9
         SYSCALL
         mov [heap_pointer], rax
-
-        ;COPY previous heap to new heap
-        mov rdi, rax
-        mov rsi, r12
-        mov rcx, r13
-        shr rcx, 0x03
-        rep movsq
-
-        ;MUNMAP previous heap
-        mov rax, 0x0b
-        mov rdi, r12
-        mov rsi, r13
-        SYSCALL
-
-        mov rcx, r8
-
-        end_resize_heap:
-
     ret
+
     insert_to_heap:                         ;This will use rax as the value to insert
     ;we are free to use rbx, and rdx
-        mov rbx, [heap_size]
-        add rbx, 0x01
-        mov [heap_size], rbx
-        imul rbx, rbx, 0x08
+    ;CHECK IF WE NEED TO RESIZE HEAP
 
+        mov rbx, [heap_size]
+        inc rbx
+        mov [heap_size], rbx
         mov rdx, [heap_capacity]
         cmp rbx, rdx
         jge resize_heap_call
@@ -177,70 +161,13 @@ section .text                                       ; declaring section .text wh
             call resize_heap
             jmp end_resize_heap_call
         end_resize_heap_call:
-            mov rbx, [heap_size]
-            sub rbx, 0x01
-            imul rbx, rbx, 0x08
-            mov rdx, rbx
-            add rdx, [heap_pointer]
-            mov qword [rdx], rax
-
-            call move_up_the_heap
 
     ret
 
-    move_up_the_heap:                       ;This needs to have rdx = index of the element we want to move up
-
-        sub rbx, 0x08
-        sar rbx, 0x01
-
-        mov r8, [heap_size]
-        and r8, 0x01
-        add r8, 0x00
-        je even_index
-        jmp odd_index
-        odd_index:
-            sub rbx, 0x04
-        even_index:
-
-        start_move_up_loop:
-            cmp rbx, 0x00
-            jl end_move_up_loop
-            mov r8, [heap_pointer]
-            add r8, rbx
-            mov r9, [r8]
-            cmp r9, rax
-            jle end_move_up_loop
-
-
-            test_breakpoint:
-            mov [r8], rax
-            mov [rdx], r9
-            mov rdx, r8
-
-            sub rbx, 0x08
-            sar rbx, 0x01
-
-            mov r8, [heap_size]
-            and r8, 0x01
-            add r8, 0x00
-            jne even_index_in_loop
-            jmp odd_index_in_loop
-            odd_index_in_loop:
-                sub rbx, 0x04
-            even_index_in_loop:
-
-            jmp start_move_up_loop
-
-        end_move_up_loop:
-
+    move_up_the_heap:                       ;This needs to have rax = index of the element we want to move up
     ret
 
-    move_down_the_heap:                     ;This needs to have rdx = index of the element we want to move down
-        ;TO IMPLEMENT IN THE FUTURE (THIS METHOD IS NEEDED FOR DELETING ELEMENTS FROM HEAP)
-    ret
-
-    delete_min_from_heap:                   ;This will delete the minimum element from the heap
-        ;TO IMPLEMENT IN THE FUTURE (THIS METHOD IS NEEDED FOR DELETING ELEMENTS FROM HEAP)
+    move_down_the_heap:                     ;This needs to have rax = index of the element we want to move down
     ret
 
 ;NOTE: functions always needs to have ret at the end
